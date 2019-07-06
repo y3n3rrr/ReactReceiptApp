408147fd80cf6c50cb56f97d2bf1001c9c9e0991
Mehmet Yener YILMAZ <yilmazmehmetyener@gmail.com>
2019-07-07 01:20:57 +0300

    nav menu is ok

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Button } from 'react-native'
 import {connect} from 'react-redux'
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 
 
 import DummyIlacData from '../../helpers/DummyIlacData'
+import t from 'tcomb-form-native'; // 0.6.9
+
+const Form = t.form.Form;
+
+const User = t.struct({
+  email: t.String,
+  username: t.String,
+  password: t.String,
+  terms: t.Boolean
+});
+const options = {
+  fields: {
+    terms: {
+      label: 'Agree to Terms',
+    },
+  },
+};
+
 
 
 class Dashboard extends Component {
@@ -13,12 +31,26 @@ class Dashboard extends Component {
     const ilacData = DummyIlacData();
     debugger
   }
+  handleSubmit = () => {
+    // do the things  
+    const value = this._form.getValue(); // use that ref to get the form value
+    console.log('value: ', value);
+
+  }
+
   render() {
     return (
       <View>
-        <Text> You are in dashboard</Text>
-        <FormLabel>Name</FormLabel>
-        <FormValidationMessage>Error message</FormValidationMessage>
+         <Form 
+          ref={c => this._form = c} // assign a ref
+          type={User} 
+          options={options} // pass the options via props
+
+        />
+        <Button
+          title="Sign Up!"
+          onPress={this.handleSubmit}
+        />
       </View>
     )
   }
